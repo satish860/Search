5f970643d152ff99618f3025d0a88df322af25aa
satish <satish860@gmail.com>
2022-04-13 14:27:03 +0530

    Update Device.cs

--- src/Search.Domain/Device.cs
+++ src/Search.Domain/Device.cs
@@ -1,7 +1,55 @@
 ﻿namespace Search.Domain
 {
-    public class Class1
+    
+    public class Device
     {
+        public string Id { get; set; }
+
+        public string DisplayModel { get; set; }
+
+        public string FriendlyName { get; set; }
+
+        public string Name { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public string SerialNumber { get; set; }
+
+        public string HardwareIdentifier { get; set; }
+
+        public int LocationGroupID { get; set; }
+
+        public string AssetNumber { get; set; }
+
+        public string UserName { get; set; }
+
+        public string Domain { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string MiddleName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string FullName { get; set; }
+
+        public string EmailAddress { get; set; }
+
+        public string EmployeeIdentifier { get; set; }
+
+        public string CostCenter { get; set; }
+
+        public string LocationGroupName { get; set; }
+
+        public string MACAddress { get; set; }
+
+        public string CurrentIPAddress { get; set; }
+
+        public int? CorpLiable { get; set; }
+
+        public string OSVersion { get; set; }
+
+        public string DeviceType { get; set; }
 
     }
 }
